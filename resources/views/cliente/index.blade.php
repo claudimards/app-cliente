86415befd3c039bed9a11d03897023fa3904e63b
claudimards <claudimards@gmail.com>
2020-07-16 23:32:01 -0300

    Incluido paginação eo recuperar dados do banco, e adionada tabela populando com clientes do banco

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ __('Lista de Clientes') }}</div>
 
@@ -14,11 +14,39 @@
                         </div>
                     @endif
 
-                    {{ __('View lista de clientes') }}
+                    <p>
+                        <a class="btn btn-primary" href="#">Adicionar</a>
+                    </p>
+                    
+                    <table class="table table-bordered">
+                        <thread>
+                            <tr>
+                                <th>#</th>
+                                <th>Nome</th>
+                                <th>E-mail</th>
+                                <th>Endereço</th>
+                                <th>Ação</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($clientes as $cliente)
+                            <tr>
+                                <th scope="row">{{ $cliente->id }}</th>
+                                <td>{{ $cliente->nome }}</td>
+                                <td>{{ $cliente->email }}</td>
+                                <td>{{ $cliente->endereco }}</td>
+                                <td>
+                                    <a class="btn btn-info" href="#">Editar</a>
+                                    <a class="btn btn-danger" href="#">Deletar</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
 
-                    @foreach($clientes as $cliente)
-                        <li>{{ $cliente->nome }}</li>
-                    @endforeach
+                    <div class="row justify-content-center">
+                        {!! $clientes->links() !!}
+                    </div>
 
                 </div>
             </div>
